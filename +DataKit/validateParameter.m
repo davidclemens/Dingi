e4884e06ed36ffefd7dd57bdc6136861919f968e
David Clemens <daclem@gmx.de>
2020-10-14 12:32:22 +0200

    Add parameter unit testing
    When matching parameters the unit should also match. Thus the unit is
    now checked against the regular expression in the UnitRegexp column in
    the validateParameters.xlsx file.

--- +DataKit/validateParameter.m
+++ +DataKit/validateParameter.m
@@ -1,8 +1,7 @@
-function [parameterIsValid,varargout] = validateParameter(parameter)
+function [parameterIsValid,varargout] = validateParameter(parameter,varargin)
 % VALIDATEPARAMETER
     
     nargoutchk(0,2)
-    
     
 	% input check: parameter
     if ischar(parameter)
@@ -13,32 +12,63 @@ function [parameterIsValid,varargout] = validateParameter(parameter)
     end
     nRequestedParameters  	= numel(parameter);
     
-    
-    validParameters         = DataKit.importTableFile([getToolboxRessources('DataKit'),'/validParameters.xlsx']);
- 
-    maskRequestedParameters = false(size(validParameters,1),nRequestedParameters);
-    for par = 1:nRequestedParameters
-        maskRequestedParameters(:,par)	= ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Abbreviation'}),parameter{par})) | ...
-                                          ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Symbol'}),parameter{par})) | ...
-                                          ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Parameter'}),parameter{par}));
-    end
+	% parse Name-Value pairs
+    optionName          = {'Unit'}; % valid options (Name)
+    optionDefaultValue  = {repmat({'<unspecified>'},nRequestedParameters,1)}; % default value (Value)
+    [Unit]              = internal.stats.parseArgs(optionName,optionDefaultValue,varargin{:}); % parse function arguments
 
-    nMatches                    = sum(maskRequestedParameters)';
-    parameterHasMultipleMatches	= nMatches > 1;
+    % input check: Unit
+	if ischar(Unit)
+        Unit	= cellstr(Unit);
+    elseif ~iscellstr(Unit)
+        error('DataKit:validateParameter:invalidUnitType',...
+              'The units of parameter(s) to validate have to be specified as a char or cellstr.')
+    end
+    if numel(Unit) ~= nRequestedParameters
+        error('DataKit:validateParameter:sameNumberOfUnitsAndParametersRequired',...
+            'The same number of units and parameters have to be specified')
+    end
+    unitsProvided   = ~all(ismember(Unit,'<unspecified>'));
+    
+    % load remote table
+    validParameters	= DataKit.importTableFile([getToolboxRessources('DataKit'),'/validParameters.xlsx']);
+ 
+    maskRequestedParameters         = false(size(validParameters,1),nRequestedParameters);
+    maskRequestedParametersUnits    = false(size(validParameters,1),nRequestedParameters);
+    for par = 1:nRequestedParameters
+        % match parameter name
+        maskRequestedParameters(:,par)   	= ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Abbreviation'}),parameter{par})) | ...
+                                              ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Symbol'}),parameter{par})) | ...
+                                              ~cellfun(@isempty,regexpi(cellstr(validParameters{:,'Parameter'}),parameter{par}));
+        % match parameter unit
+        if unitsProvided
+            maskRequestedParametersUnits(:,par)	= ~cellfun(@isempty,cellfun(@(reg) regexp(Unit{par},reg),validParameters{:,'UnitRegexp'},'un',0));
+        else
+            maskRequestedParametersUnits        = true(size(validParameters,1),nRequestedParameters);
+        end
+    end
+    
+    % Units must match
+    maskRequestedParameters     = maskRequestedParameters & maskRequestedParametersUnits;
+    
+    runTests()
+
     
     
-    while any(parameterHasMultipleMatches)
-        idx         = find(parameterHasMultipleMatches,1);
-        matchIndex  = find(maskRequestedParameters(:,idx));
+    while any(parameterIssue)
+        issueIdx   	= find(parameterHasMultipleMatches,1);
+        matchIndex  = find(maskRequestedParameters(:,issueIdx));
         
-        fprintf('There were %g matches for parameter ''%s'':\n\n',nMatches(idx),parameter{idx})
-        disp(cat(2,table((1:nMatches(idx))','VariableNames',{'Index'}),validParameters(maskRequestedParameters(:,idx),:)))
-        prompt  = 'Enter index of the desired match [1] or cancel [c]: ';
+        fprintf('There were %g matches for parameter ''%s'' with unit ''%s'':\n\n',nMatches(issueIdx),parameter{issueIdx},Unit{issueIdx})
+        disp(cat(2,table((1:nMatches(issueIdx))','VariableNames',{'Index'}),validParameters(maskRequestedParameters(:,issueIdx),:)))
+        prompt  = 'Enter index of the desired match [1], ignore [i] or cancel [c]: ';
         str     = input(prompt,'s');
         if isempty(str)
             selectionIndex = 1;
         else
-            if strcmpi(str,'c')
+            if strcmpi(str,'i')
+                selectionIndex = [];
+            elseif strcmpi(str,'c')
 
             else
                 selectionIndex = str2double(str);
@@ -47,10 +77,9 @@ function [parameterIsValid,varargout] = validateParameter(parameter)
         
         tmpMask     = false(size(validParameters,1),1);
         tmpMask(matchIndex(selectionIndex)) = true;
-        maskRequestedParameters(:,idx) = tmpMask;
+        maskRequestedParameters(:,issueIdx) = tmpMask;
         
-        nMatches                    = sum(maskRequestedParameters)';
-        parameterHasMultipleMatches	= nMatches > 1;
+        runTests()
     end
     
     parameterIsValid           	= (sum(maskRequestedParameters) == 1)';
@@ -65,4 +94,20 @@ function [parameterIsValid,varargout] = validateParameter(parameter)
     info                        = info(1:end - 1,:);
     
     varargout{1}    = info;
+    
+    function runTests()
+        nMatches                    = sum(maskRequestedParameters)';
+        parameterHasMultipleMatches	= nMatches > 1;
+        
+%         if unitsProvided
+%             maskRequestedParametersWithUnit     = maskRequestedParameters & maskRequestedParametersUnits;
+%             nMatchesWithUnit                    = sum(maskRequestedParametersWithUnit)';
+%         else
+%             nMatchesWithUnit                    = true(size(nMatches));
+%         end
+%         parameterWithUnitHasMultipleMatches	= nMatchesWithUnit > 1;
+%         
+%         unitIssues      = nMatches == 1 & nMatchesWithUnit == 0;
+        parameterIssue  = parameterHasMultipleMatches;
+    end
 end
