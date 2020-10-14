74ebce9310d3c81e7109545dfb14dd07c39bde40
David Clemens <daclem@gmx.de>
2020-10-14 12:34:38 +0200

    Fix to find all column names
    Previously not all column names were necessarily found resulting in
    shifts of the data entries.

--- +GearKit/@sensor/readSeabirdCTD.m
+++ +GearKit/@sensor/readSeabirdCTD.m
@@ -33,8 +33,17 @@ function obj = readSeabirdCTD(obj)
     
     headerRawText 	= textscan(fId,'%s',nHeaderLines,...
                         'Delimiter',         	'\n');
-	tmpVars         = regexp(headerRawText{:},'^# name \d+ = (?<VarNameShort>.+):\s{1,2}(?<VarNameLong>[A-Za-z\d\+\-\.\s]+)\,?\s(?<VarDescriptor>[A-Za-z\s]+)?\s?(?<VarUnits>\[.+\])?$','names');
-	tmpVars         = cat(1,tmpVars{:});
+                    
+	isVariable      = regexp(headerRawText{:},'^# name \d+ = ');
+    isVariable      = ~cellfun(@isempty,isVariable);
+    nVars           = sum(isVariable);
+	tmpVars         = regexp(headerRawText{:},'^# name \d+ = (?<VarNameShort>.+):\s{1,2}(?<VarNameLong>[A-Za-z\d\+\-\.\s]+)\,?\s(?<VarDescriptor>.+?)?\s?(?<VarUnits>\[.+\])$','names');
+    hasToken        = ~cellfun(@isempty,tmpVars);
+	tmpVars         = cat(1,tmpVars{isVariable});
+    emptyValue      = repmat({''},nVars,1);
+    tmpVariables    = struct('VarNameShort',emptyValue,'VarNameLong',emptyValue,'VarDescriptor',emptyValue,'VarUnits',emptyValue);
+    tmpVariables(hasToken(isVariable)) = tmpVars;
+    
 	tmpStartTime    = regexp(headerRawText{:},'# start_time = (.+) \[.+\]','tokens');
 	tmpStartTime  	= cat(1,tmpStartTime{:});
 	tmpBadFlag      = regexp(headerRawText{:},'# bad_flag = (.+)','tokens');
@@ -42,30 +51,42 @@ function obj = readSeabirdCTD(obj)
     tmpHardware   	= regexp(headerRawText{:},'<HardwareData DeviceType=''(?<DeviceType>.+)''\sSerialNumber=''(?<SerialNumber>\d+)''>','names');
 	tmpHardware   	= cat(1,tmpHardware{:});
     
-    varNames        = struct2table(tmpVars);
+    varNames        = struct2table(tmpVariables);
     varNames{:,'VarNameLong'}   = regexprep(lower(varNames{:,'VarNameLong'}),'\s([a-z])','${upper($1)}');
     varNames{:,'VarUnits'}      = regexprep(varNames{:,'VarUnits'},'^\[(.+)\]$','$1');
     
     startTime       = datetime(tmpStartTime{:},'InputFormat','MMM dd yyyy HH:mm:ss');
 	badFlag         = str2double(tmpBadFlag{:}{:});
-	nVars           = numel(tmpVars);
                     
     rawText         = textscan(fId,[repmat('%f',1,nVars),'%f'],...
                         'Delimiter',         	' ',...
                         'MultipleDelimsAsOne',  true,...
-                        'EmptyValue',           badFlag);
+                        'EmptyValue',           NaN);
                     
     fclose(fId);
-                
+	
+    rawTextBadMask  = cellfun(@(d) d - badFlag <= 1e-10,rawText,'un',0);
+    for ii = 1:numel(rawText)
+        rawText{ii}(rawTextBadMask{ii}) = NaN;
+    end
+    
     obj.type                    = 'ctd';
-    obj.serialNumber            = tmpHardware.SerialNumber;
-    obj.time                    = datenum(startTime + duration(0,0,rawText{1}) - duration(0,0,rawText{1}(1)));
-    obj.data                    = cat(2,rawText{2:end - 1});
+    if numel(tmpHardware) > 0
+        obj.serialNumber            = tmpHardware.SerialNumber;
+    else
+        
+    end
+    
+    parameters                  = varNames{1:end,'VarNameLong'};
+    [isValid,info]              = DataKit.validateParameter(parameters,...
+                                    'Unit',     varNames{1:end,'VarUnits'});
+    
+    timeInd                     = find(~cellfun(@isempty,regexpi(parameters,'time')),1);
+    obj.time                    = datenum(startTime + duration(0,0,rawText{timeInd}) - duration(0,0,rawText{timeInd}(1)));
+    obj.data                    = cat(2,rawText{1:numel(parameters)});
   	obj.timeInfo.name           = 'time';
   	obj.timeInfo.timeZone       = 'UTC';
     
-    parameters                  = varNames{2:end,'VarNameLong'};
-    [isValid,info]              = DataKit.validateParameter(parameters);
     if ~all(isValid)
         warning('GearKit:sensor:readSeabirdCTD:unrecognizedParameter',...
                 'The parameter(s):\n\t''%s''\nare not recognized in the DataKit toolbox. It is not imported.\n',strjoin(parameters(~isValid),'\n\t'))
