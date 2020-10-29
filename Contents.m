cdccebac8adcdd6fd0b13eed4c114235440a1989
David Clemens <daclem@gmx.de>
2020-10-29 10:55:51 +0100

    First Contents.m file
    This commit is part of #6.

---
+++ Contents.m
@@ -0,0 +1,12 @@
+% TOOLBOXES
+% Version 0.1b1 29-Oct-2020
+%
+% Toolboxes
+%   AnalysisKit - analyse your data
+%   DataKit - manipulate your data
+%   DebuggerKit - tools for debugging
+%   GearKit - scientific gear
+%   GraphKit - graphing tools
+%
+% Files
+%   getToolboxRessources - return the path to the specified toolbox
