51bbe84c2b47d7e1284d00f1e6e280eb5a6af764
David Clemens <daclem@gmx.de>
2020-10-29 09:52:35 +0100

    Fix wrong unit conversions

--- +GearKit/@bigoDeployment/determineChamberMetadata.m
+++ +GearKit/@bigoDeployment/determineChamberMetadata.m
@@ -8,7 +8,7 @@ function obj = determineChamberMetadata(obj)
     indRecoveryTable    = find(all(RecoveryTable{:,{'Cruise','Gear'}} == [obj.cruise,obj.gear],2));
     maskHeights         = ~cellfun(@isempty,regexp(RecoveryTable.Properties.VariableNames,'ChH\d+'));
     
-    chamberArea       	= pi.*(RecoveryTable{indRecoveryTable,'ChDia'}./2).^2.*1e-3; % m^2
+    chamberArea       	= pi.*(RecoveryTable{indRecoveryTable,'ChDia'}./2.*1e-2).^2; % m^2
     chamberHeight       = nanmean(RecoveryTable{indRecoveryTable,maskHeights},2); % cm
     
     chamberData         = struct();
@@ -22,7 +22,7 @@ function obj = determineChamberMetadata(obj)
         ind     = ind + 1;
 
         chamberData.(sg)(ind).Parameter    = 'volumeViaHeight';
-        chamberData.(sg)(ind).Value        = chamberArea(ch)*1e2*chamberHeight(ch)*1e-2;
+        chamberData.(sg)(ind).Value        = chamberArea(ch).*1e2.*chamberHeight(ch).*1e-1;
         chamberData.(sg)(ind).Unit         = 'L';
         ind     = ind + 1;
 
