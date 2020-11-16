3c374cd927eb9da896b9315b1dbc67e5cf1266bd
David Clemens <daclem@gmx.de>
2020-11-16 22:30:11 +0100

    Improve getToolboxRessources function

--- getToolboxRessources.m
+++ getToolboxRessources.m
@@ -1,7 +1,27 @@
-function [path,varargout] = getToolboxRessources(toolboxName)
-    packageInfo     = what(toolboxName);
-    path            = [packageInfo.path,'/ressources'];
-    files           = dir(path);
+function [path,varargout] = getToolboxRessources(toolboxName,option)
+
+    if exist('option','var') == 1
+        if ~ischar(option)
+            error('getToolboxRessources:invalidOption',...
+                '''option'' has to be a valid char vector.')
+        else
+            option = validatestring(option,{'parent','toolbox'});
+        end
+    else
+        option = '';
+    end
     
+    packageInfo     = what(toolboxName);
+    switch option
+        case 'parent'
+            path    = packageInfo.path;
+            parts   = strsplit(packageInfo.path,filesep);
+            path    = [filesep,fullfile(parts{1:end - 1})];
+        case 'toolbox'
+            path    = packageInfo.path;
+        otherwise
+            path    = [packageInfo.path,'/ressources'];
+    end
+    files           = dir(path);
     varargout{1}    = files;
 end
