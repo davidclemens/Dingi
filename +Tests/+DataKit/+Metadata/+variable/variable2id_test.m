9ddd591ca60bb829ed930bb115924823dde03daf
David Clemens <daclem@gmx.de>
2020-12-03 16:30:10 +0100

    Add a unit test
    Part of #9.

---
+++ +Tests/+DataKit/+Metadata/+variable/variable2id_test.m
@@ -0,0 +1,53 @@
+classdef variable2id_test < matlab.unittest.TestCase
+    
+    % run:
+    % tests = matlab.unittest.TestSuite.fromClass(?Tests.DataKit.Metadata.variable.variable2id_test);
+    % run(tests)
+    
+    properties
+        
+    end
+    properties (MethodSetupParameter)
+        
+    end
+    properties (TestParameter)
+        
+    end
+    
+    methods (TestClassSetup)
+        function addDataPoolClassToPath(testCase)
+            pathOld = path;
+            testCase.addTeardown(@path,pathOld)
+            addpath('/Users/David/Dropbox/David/Syncing/MATLAB/toolboxes/')
+        end
+    end
+    methods (TestMethodSetup)
+        
+    end
+    methods (TestMethodTeardown)
+        
+    end
+    
+    methods (Test)
+        function invalidVariableId(testCase)
+            
+            variableIdUnknown   = 22;
+            testCase.verifyError(@() DataKit.Metadata.variable.validateId(variableIdUnknown),...
+                'DataKit:Metadata:variable:variableFromId:invalidVariableId');
+        end
+        function validVariableId(testCase)
+            
+            variableIdKnown   = 20;
+            act     = DataKit.Metadata.variable.validateId(variableIdKnown);
+            exp     = true;
+            testCase.verifyEqual(act,exp)            
+        end
+        
+        function invalidVariableIdDataType(testCase)
+            
+            variableId   = '20';
+            testCase.verifyError(@() DataKit.Metadata.variable.validateId(variableId),...
+                'DataKit:Metadata:variable:validateId:invalidIdDataType');        
+        end
+	end
+end
