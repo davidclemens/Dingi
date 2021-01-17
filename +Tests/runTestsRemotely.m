73ce6fa3ce889341c768e2c513a2fd017c710e79
David Clemens <daclem@gmx.de>
2021-01-17 23:17:17 +0100

    Add test coverage output
    Part of #23

--- +Tests/runTestsRemotely.m
+++ +Tests/runTestsRemotely.m
@@ -1,10 +1,19 @@
 
 import matlab.unittest.TestSuite
+import matlab.unittest.TestRunner
+import matlab.unittest.plugins.CodeCoveragePlugin
+import matlab.unittest.plugins.codecoverage.CoberturaFormat
 
 try
     tests   = TestSuite.fromPackage('Tests',...
                 'IncludingSubpackages', true);
-    result  = run(tests);
+    runner	= TestRunner.withTextOutput;
+    plugin 	= CodeCoveragePlugin.forPackage(package{pp},...
+            	'Producing',            'cobertura.xml');
+        
+	runner.addPlugin(plugin)
+    result  = runner.run(tests);
+    
     display(result);
 catch ME
     disp(getReport(ME,'extended'));
