6414aaf9394ec4d6f5eae45aa677590ed4e9749e
David Clemens <daclem@gmx.de>
2021-01-25 09:37:48 +0100

    Minor improvments

--- +DataKit/@enum/core_fromProperty.m
+++ +DataKit/@enum/core_fromProperty.m
@@ -1,6 +1,10 @@
 function obj = core_fromProperty(classname,propertyname,value)
 
     validProperties     = properties(classname);
+    if isempty(validProperties)
+        error('Dingi:DataKit:enum:core_fromProperty:noAttributesAvailable',...
+            'There are no attributes defined for class ''%s''.',classname)
+    end
     propertyname        = validatestring(propertyname,validProperties);
     
     members             = enumeration(classname);
