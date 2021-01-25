0542f179087b21e0aebf05a1a89c8995f349e074
David Clemens <daclem@gmx.de>
2021-01-25 09:46:34 +0100

    Convert GearKit.worldDomain

--- +GearKit/@worldDomain/worldDomain.m
+++ +GearKit/@worldDomain/worldDomain.m
@@ -5,12 +5,16 @@ classdef worldDomain
         PelagicWaterColumn      ('PWC')
         Sediment                ('Sed')
     end
-    properties
+    properties (SetAccess = 'immutable')
         Abbreviation char
     end
     methods
         function obj = worldDomain(abbreviation,varargin)
             obj.Abbreviation = abbreviation;
         end
+    end
+    methods (Static)
+        L = listMembers()
+        obj = fromProperty(propertyname,value)
     end
 end
