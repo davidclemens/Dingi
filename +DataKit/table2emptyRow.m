9b71279816db4c9193bc2951707ae8c93d71a3c2
David Clemens <daclem@gmx.de>
2021-02-01 10:13:03 +0100

    Fix bug

--- +DataKit/table2emptyRow.m
+++ +DataKit/table2emptyRow.m
@@ -4,4 +4,10 @@ function row = table2emptyRow(tbl)
     % tbl(end + 1,:).
     tbl(end + 2,:)  = tbl(1,:);
     row             = tbl(end - 1,:);
+    
+    % Deal with the case of cellstr initialization
+    columnIsCellstr         = arrayfun(@iscellstr,table2cell(tbl(1,:)));
+    row{1,columnIsCellstr}  = {''};
+    
+    
 end
