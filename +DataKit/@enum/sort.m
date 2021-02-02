e81884680ab0303630a8af52f6290ad383029ec1
David Clemens <daclem@gmx.de>
2021-02-02 20:13:20 +0100

    Fix bug

--- +DataKit/@enum/sort.m
+++ +DataKit/@enum/sort.m
@@ -3,7 +3,7 @@ function varargout = sort(obj,varargin)
     import DataKit.enum.cell2enum
     
     objClass    = class(obj);
-    [B,I]       = sort(cellstr(obj),varargin{:});
+    [B,I]       = sort(string(cellstr(obj)),varargin{:});
     objS        = cell2enum(B,objClass);
     
     varargout{1}    = objS;
