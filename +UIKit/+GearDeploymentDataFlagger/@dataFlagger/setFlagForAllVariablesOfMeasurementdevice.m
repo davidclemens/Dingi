fe44ac5ab3c3e3136379f59673c7624785dc98d3
David Clemens <daclem@gmx.de>
2021-03-10 20:20:43 +0100

    Fix bug for applying flag to entire measurement unit

--- +UIKit/+GearDeploymentDataFlagger/@dataFlagger/setFlagForAllVariablesOfMeasurementdevice.m
+++ +UIKit/+GearDeploymentDataFlagger/@dataFlagger/setFlagForAllVariablesOfMeasurementdevice.m
@@ -18,19 +18,22 @@ function setFlagForAllVariablesOfMeasurementdevice(obj)
     % Retrieve indices and brush data
     poolIdx         = cat(1,charts{:,'userData'}{:});
     poolIdx         = poolIdx(:,1);
+    [uPoolIdx,uInd1,uInd2]         = unique(poolIdx);
     brushData       = cellfun(@find,charts{:,'brushData'},'un',0);
-    nBrushedData    = cellfun(@numel,brushData);
     
     % Get size of relevant data pools
-    nDataPools = size(charts,1);
-    nS = NaN(nDataPools,1);
-    nV = NaN(nDataPools,1);
-    for md = 1:nDataPools
-        [nS(md),nV(md)] = size(df.Deployments(df.DeploymentIsSelected).data.Data{poolIdx(md)});
+    nPools = size(uPoolIdx,1);
+    nS = NaN(nPools,1);
+    nV = NaN(nPools,1);
+    uBrushData = brushData(uInd1);
+    for md = 1:nPools
+        [nS(md),nV(md)] = size(df.Deployments(df.DeploymentIsSelected).data.Data{uPoolIdx(md)});
+        uBrushData{md}  = unique(cat(2,brushData{uInd2 == md}));
     end
+    nuBrushedData    = cellfun(@numel,uBrushData);
     
  	% Grow vectors to match data
-    poolIdxAll      = arrayfun(@(ns,nv,p) reshape(p.*ones(ns,nv),[],1),nS,nV,poolIdx,'un',0);
+    poolIdxAll      = arrayfun(@(ns,nv,p) reshape(p.*ones(ns,nv),[],1),nS,nV,uPoolIdx,'un',0);
     poolIdxAll      = cat(1,poolIdxAll{:});
     variableIdxAll	= arrayfun(@(ns,nv) reshape((1:nv).*ones(ns,1),[],1),nS,nV,'un',0);
     variableIdxAll	= cat(1,variableIdxAll{:});
@@ -38,16 +41,19 @@ function setFlagForAllVariablesOfMeasurementdevice(obj)
     sampleIdxAll    = cat(1,sampleIdxAll{:});
     
     % Grow vectors to match brush data
-    poolIdxBrushed      = arrayfun(@(ns,nv,p) reshape(p.*ones(ns,nv),[],1),nBrushedData,nV,poolIdx,'un',0);
+    poolIdxBrushed      = arrayfun(@(ns,nv,p) reshape(p.*ones(ns,nv),[],1),nuBrushedData,nV,uPoolIdx,'un',0);
     poolIdxBrushed      = cat(1,poolIdxBrushed{:});
-    variableIdxBrushed	= arrayfun(@(ns,nv) reshape((1:nv).*ones(ns,1),[],1),nBrushedData,nV,'un',0);
+    variableIdxBrushed	= arrayfun(@(ns,nv) reshape((1:nv).*ones(ns,1),[],1),nuBrushedData,nV,'un',0);
     variableIdxBrushed	= cat(1,variableIdxBrushed{:});
-    sampleIdxBrushed   	= cellfun(@(nv,b) reshape(repmat(reshape(b,[],1),1,nv),[],1),num2cell(nV),brushData,'un',0);
+    sampleIdxBrushed   	= cellfun(@(nv,b) reshape(repmat(reshape(b,[],1),1,nv),[],1),num2cell(nV),uBrushData,'un',0);
     sampleIdxBrushed	= cat(1,sampleIdxBrushed{:});
     
     % Apply flags
     df.applyFlags(poolIdxAll,sampleIdxAll,variableIdxAll,poolIdxBrushed,sampleIdxBrushed,variableIdxBrushed);
 
+    % Update UI
+    df.setBrushData;
+    
     % Set status text
     setStatusText(df,'')
 end
