03cf207ef1f005c06a356cb9032ecf5993327f3a
David Clemens <daclem@gmx.de>
2021-04-28 15:09:39 +0200

    Fix typo

--- +DataKit/@bitmask/bitmask.m
+++ +DataKit/@bitmask/bitmask.m
@@ -18,14 +18,14 @@ classdef bitmask
     %
     % Copyright (c) 2021 David Clemens (dclemens@geomar.de)
     %
-    
+
     % Frontend
     properties (Dependent)
         StorageTypeName char % Current storage type name
         Bits % Decimal bitmask array
         Size double % Bitmask size
     end
-    
+
     % Backend
     properties (Access = private)
         Bits_ = uint8.empty
@@ -40,7 +40,7 @@ classdef bitmask
         validStorageTypeNames = {'uint8','uint16','uint32','uint64'}
         validStorageTypes uint8 = 2.^(3:6)
     end
-    
+
     % Constructor method
     methods
         function obj = bitmask(varargin)
@@ -73,7 +73,7 @@ classdef bitmask
             %       set to 1. It is stored as uint16.
             %     obj = BITMASK(2,3,9,5,10) creates a 5x10 bitmask with bit 9 at index
             %       (2,3) set to 1. It is stored as uint16.
-            %     obj = BITMASK([2,1],[3,8],[2,9],5,10) creates a 5x10 bitmask with bit 
+            %     obj = BITMASK([2,1],[3,8],[2,9],5,10) creates a 5x10 bitmask with bit
             %       2 at index (2,3) and bit 9 at index (1,8) set to 1. It is stored as
             %       uint16.
             %
@@ -97,7 +97,7 @@ classdef bitmask
             %         vector. Any elements in bit that are zero are ignored, as are the
             %         corresponding subscripts in i and j. However, if you do not
             %         specify the dimension sizes of the output, m and n, then
-            %         sparseBitmask calculates the maxima m = max(i(:)) and n = max(j(:))
+            %         bitmask calculates the maxima m = max(i(:)) and n = max(j(:))
             %         before ignoring any zero elements in bit.
             %
             %     m, n - Size of each dimension (as separate arguments)
@@ -120,7 +120,7 @@ classdef bitmask
             %
             %   Copyright (c) 2021 David Clemens (dclemens@geomar.de)
             %
-            
+
             switch nargin
                 case 0
                     return
@@ -148,32 +148,32 @@ classdef bitmask
             obj = initializeBitmask(obj,i,j,bit,sz);
         end
     end
-    
+
     methods
         obj = setBit(obj,bit,highlow,varargin)
         obj = setNum(obj,num,varargin)
         tf = isBit(obj,bit)
     end
-    
+
     % Overloaded methods
     methods
         disp(obj,varargin)
         obj = subsasgn(obj,s,b)
         obj = cat(dim,varargin)
     end
-    
+
     methods (Access = private)
         obj = initializeBitmask(obj,i,j,bit,sz)
         obj = changeStorageType(obj,newStorageType)
         storageType = minStorageType(obj,A)
         obj = extendBitmask(obj,varargin)
-    end    
-    
+    end
+
     % Static methods
     methods (Access = private, Static)
         intout = setbits(bits)
     end
-    
+
     % Get methods
     methods
         function bits = get.Bits(obj)
@@ -189,7 +189,7 @@ classdef bitmask
             sz = size(obj.Bits_);
         end
     end
-    
+
     % Set methods
     methods
         function obj = set.Bits_(obj,newNum)
@@ -205,35 +205,35 @@ classdef bitmask
             obj.Bits_           = newNum;
         end
         function obj = set.Bits(obj,newNum)
-            
+
             % Determine smallest storage type which holds all information of array
             % 'value'. And make this the new storage type.
             obj = obj.setNum(newNum);
         end
         function obj = set.StorageTypeName(obj,value)
-            
+
             % To avoid load order dependeces, the 'StorageTypeName' property is a dependent
             % property, which retrieves the value of the property 'StorageTypeName_'.
             % Search for 'Avoid Property Initialization Order Dependency' in the
             % documentation.
-            
+
             newStorageTypeName = validatestring(value,obj.validStorageTypeNames);
-            
+
             obj = changeStorageType(obj,newStorageTypeName);
         end
         function obj = set.StorageType(obj,value)
-            
+
             % To avoid load order dependeces, the 'StorageType' property is a dependent
             % property, which retrieves the value of the property 'StorageType_'.
             % Search for 'Avoid Property Initialization Order Dependency' in the
             % documentation.
-            
+
             if ismember(value,obj.validStorageTypes)
                 newStorageTypeName = value;
             else
                 error('Invalid storage type ''%u''. Valid types are: %s.',value,strjoin(cellstr(string(2.^(3:6))),', '))
             end
-            
+
             obj = changeStorageType(obj,newStorageTypeName);
         end
     end
