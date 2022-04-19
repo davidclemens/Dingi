f7545a77eeed0ce23b97aa5321c5a33182cb015e
David Clemens <daclem@gmx.de>
2022-04-19 09:26:00 +0200

    Add documentation templates

---
+++ ressources/templates/TemplateDocumentationClass.m
@@ -0,0 +1,18 @@
+classdef ClassName
+    % ClassName  Short description of the class ClassName
+    % This is then the long description of the class ClassName. It can span
+    % multiple lines and really describes the class' capabilities.
+    %
+    % ClassName Properties:
+    %   property1 - Short description of property property1
+    %   property2 - Short description of property property2
+    %
+    % ClassName Methods:
+    %   method1 - Short description of method method1
+    %   method2 - Short description of method method2
+    %
+    % Copyright (c) 2021 David Clemens (dclemens@geomar.de)
+    %
+
+
+end
