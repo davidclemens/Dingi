a7a2b9e0d9136176db8558d050cf4ce91bcfa606
David Clemens <daclem@gmx.de>
2022-05-17 10:27:12 +0200

    Improve version retrieval

--- getToolboxVersion.m
+++ getToolboxVersion.m
@@ -1,10 +1,5 @@
 function version = getToolboxVersion()
-
-    toolboxInfo = what('Dingi');
-
-    fId     = fopen([toolboxInfo.path,'/.version'],'r');
-    raw     = textscan(fId,'%s',1);
-    fclose(fId);
-
-    version = raw{1}{1};
+    
+    info    = ver('Dingi');
+    version = info.Version;
 end
