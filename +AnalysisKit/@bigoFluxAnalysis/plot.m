d99582d27dc174bc2f496730ec2f28f01d33c559
David Clemens <daclem@gmx.de>
2022-09-20 00:09:57 +0200

    Fix bug

--- +AnalysisKit/@bigoFluxAnalysis/plot.m
+++ +AnalysisKit/@bigoFluxAnalysis/plot.m
@@ -180,7 +180,7 @@ function varargout = parseInputs(obj,varargin)
     % Define validationFunctions
     validateObj = @(x) isa(x,'AnalysisKit.bigoFluxAnalysis');
     validatePlotType = @(x) ~isempty(validatestring(x,validPlotTypes));
-    validateVariable = @(x) validateattributes(x,{'char','cell'});
+    validateVariable = @(x) validateattributes(x,{'char','cell'},{'nonempty'});
     validateShowConfidenceInterval = @(x) validateattributes(x,{'logical'},{'scalar','nonempty'});
 
     % Create input parser
