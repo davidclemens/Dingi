edd4d715f64b07490ca5e42a3058a1b340aee899
David Clemens <daclem@gmx.de>
2022-11-11 20:20:52 +0100

    Fix size bug

--- +DataKit/@bitmask/size.m
+++ +DataKit/@bitmask/size.m
@@ -3,14 +3,16 @@ function varargout = size(obj,varargin)
     narginchk(1,2)
     
     nDims   = ndims(obj.Bits);
-    nargoutchk(0,nDims)
     
-    if numel(varargin) == 0
-        sz = cell(1,nDims);
-    else
-        sz = cell(1);
+    sz = size(obj.Bits,varargin{:});
+    
+    if nargout <= 1
+        varargout{1} = sz;
+    elseif nargout >= 2
+        if nargout < nDims
+            varargout = num2cell([sz(1:nargout - 1),prod(sz(nargout:end))]);
+        else
+            varargout = num2cell([sz,ones(1,nargout - nDims)]);
+        end
     end
-    [sz{:}] = size(obj.Bits,varargin{:});
-    
-    varargout = sz;
 end
