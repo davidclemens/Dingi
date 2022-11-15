f301c093caea83869b7489a2a43a13e006825d3e
David Clemens <daclem@gmx.de>
2022-11-15 21:40:45 +0100

    Add getDisplayFloatFormats documentation

--- +DataKit/@quantity/getDisplayFloatFormats.m
+++ +DataKit/@quantity/getDisplayFloatFormats.m
@@ -1,7 +1,47 @@
 function [dblFmt,snglFmt] = getDisplayFloatFormats()
-% Display for double/single will follow 'format long/short g/e' or 'format bank'
-% from the command window. 'format long/short' (no 'g/e') is not supported
-% because it often needs to print a leading scale factor.
+% getDisplayFloatFormats  Get current float display format setting
+%   GETDISPLAYFLOATFORMATS get the currently active float value display format.
+%
+%   Syntax
+%     dblFmt = GETDISPLAYFLOATFORMATS()
+%     [dblFmt,snglFmt] = GETDISPLAYFLOATFORMATS()
+%
+%   Description
+%     dblFmt = GETDISPLAYFLOATFORMATS()  Return the current display format for
+%       double-precision values.
+%     [dblFmt,snglFmt] = GETDISPLAYFLOATFORMATS()  Additionally return the
+%       current display format for single-precision values.
+%
+%   Example(s)
+%     [dblFmt,snglFmt] = GETDISPLAYFLOATFORMATS()  dblFmt = '%.5g' & 
+%       snglFmt = '%.5g'.
+%
+%
+%   Input Arguments
+%
+%
+%   Output Arguments
+%     dblFmt - Double display format
+%       char row vector
+%         The double-precision format specifier set for the command window. This
+%         follows the 'format style' syntax for setting the command window
+%         output display format.
+%
+%     snglFmt - Single display format
+%       char row vector
+%         The single-precision format specifier set for the command window. This
+%         follows the 'format style' syntax for setting the command window
+%         output display format.
+%
+%
+%   Name-Value Pair Arguments
+%
+%
+%   See also 
+%
+%   Copyright (c) 2022-2022 David Clemens (dclemens@geomar.de)
+%
+
     switch lower(matlab.internal.display.format)
         case {'short' 'shortg' 'shorteng'}
             dblFmt  = '%.5g';
