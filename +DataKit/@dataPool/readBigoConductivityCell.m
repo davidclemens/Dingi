baa4de0f9d4bd55af416ab1d1eda20bad5c1104d
David Clemens <daclem@gmx.de>
2022-12-06 14:32:49 +0100

    Fix bug

--- +DataKit/@dataPool/readBigoConductivityCell.m
+++ +DataKit/@dataPool/readBigoConductivityCell.m
@@ -1,20 +1,56 @@
 function varargout = readBigoConductivityCell(obj,path)
 
+    import DebuggerKit.Debugger.printDebugMessage
+    
     nargoutchk(0,1)
 
     fId             = fopen(path,'r');
     if fId == -1
-        warning('Dingi:DataKit:dataPool:readBigoConductivity:unableToOpenFile',...
+        warning('Dingi:DataKit:dataPool:readBigoConductivityCell:UnableToOpenFile',...
                 'Unable to read data in:\n\t ''%s''',path)
         return
     end
-
-    rawText         = textscan(fId,'%{MM-dd-yyyy}D%{HH:mm:ss}D%*q%s%f%f%f%*f%*f',...
+    
+    % Read header line and count the number of columns
+    headerLine  = fgetl(fId);
+    if isnumeric(headerLine) && headerLine == -1
+        error('Dingi:DataKit:dataPool:readBigoConductivityCell:EmptyFile',...
+            'The conductivity file ''%s'' is empty.',path)
+    end
+    header          = textscan(headerLine,'%s',...
+                        'Delimiter',	'\t');
+    header          = header{1}';
+    nHeaderColumns	= numel(header);
+    
+    % Read first data line and count the number of columns
+    dataLine    = fgetl(fId);
+    frewind(fId); % Return to the beginning of the file for subsequent reading
+    if isnumeric(headerLine) && headerLine == -1
+        error('Dingi:DataKit:dataPool:readBigoConductivityCell:NoDataFile',...
+            'The conductivity file ''%s'' has no data.',path)
+    end
+    data            = textscan(dataLine,'%s',...
+                        'Delimiter',	'\t');
+    data            = data{1}';
+    nDataColumns	= numel(data);
+    
+    % Handle column count mismatch errors
+    if nHeaderColumns < nDataColumns
+        printDebugMessage('Dingi:DataKit:dataPool:readBigoConductivityCell:ColumnCountMismatch',...
+            'Warning','The conductivity file ''%s'' has more data columns (%u) than header columns (%u). Only the first %u columns are processed further.',path,nDataColumns,nHeaderColumns,nHeaderColumns)
+    elseif nHeaderColumns > nDataColumns
+        printDebugMessage('Dingi:DataKit:dataPool:readBigoConductivityCell:ColumnCountMismatch',...
+            'Warning','The conductivity file ''%s'' has more header columns (%u) than data columns (%u). Only the first %u columns are processed further.',path,nHeaderColumns,nDataColumns,nDataColumns)
+    end
+    
+    % Build format spec and read entire file
+    formatSpec = ['%{MM-dd-yyyy}D%{HH:mm:ss}D%*q%s%f%f%f',repmat('%*q',1,nDataColumns - 7)];
+    rawText         = textscan(fId,formatSpec,...
                         'Delimiter',         	'\t',...
                         'MultipleDelimsAsOne',  false,...
                         'HeaderLines',          1);
     fclose(fId);
-
+    
     measuringDevice                 = GearKit.measuringDevice();
     measuringDevice.Type            = 'BigoConductivityCell';
     measuringDevice.SerialNumber  	= rawText{3}{1};
